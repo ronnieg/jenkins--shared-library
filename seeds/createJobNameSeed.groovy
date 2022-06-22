a941f8abed95b3af4e08b32d69c442fc23acef08
Yahor Bukatkin <yahor.bukatkin@kyriba.com>
2022-06-22 13:04:14 +0200

    Refactoring

---
+++ seeds/createJobNameSeed.groovy
@@ -0,0 +1,27 @@
+createJob(this, 'GET_FIRST_NAME')
+
+static createJob(def dslFactory, def name) {
+    dslFactory.freeStyleJob(name) {
+
+        parameters {
+            stringParam('NAME', 'Vasya', 'My First name')
+        }
+
+        label "build"
+
+        wrappers {
+            preBuildCleanup()
+            buildName('nam: ${Env.NAME}')
+        }
+
+        steps {
+            shell("Hello ${Env.Name}")
+        }
+
+        publishers {
+            wsCleanup {
+                deleteDirectories(true)
+            }
+        }
+    }
+}
